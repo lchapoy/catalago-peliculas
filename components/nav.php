06401536e309c9ebe0531d6f7ace38e3afdc7918
lchapoy <luis_chapoy@hotmail.com>
2019-06-22 16:23:24 -0500

    add functions, class, array manipulation, while loop to nav.php

--- components/nav.php
+++ components/nav.php
@@ -3,27 +3,58 @@
     $path = parse_url($directoryURI, PHP_URL_PATH);
     $components = explode('/', $path);
     $first_part = end($components);
+    class Route{
+        private $ruta;
+        private $role;
+        function __construct($ruta, $label, $role) { 
+            $this->ruta = $ruta;
+            $this->role = $role;
+            $this->label = $label;
+        }
+        //Aquí van los métodos
+        public function rutaActiva($rutaActiva){
+            return $this->ruta === $rutaActiva;
+        }
+        public function permisoRole(){
+            return in_array($_SESSION["role"] , $this->role);
+        }
+        public function obtenerRuta(){
+            return $this->ruta;
+        }
+        public function getListElement($first_part) {
+            if ($this->permisoRole()){
+                $active = $first_part === $this->ruta ? 'active' : '';
+                $class = "nav-item ".$active;
+                return '<li class="'.$class.'"><a class="nav-link" href="'.$this->ruta.'">'.$this->label.'</a></li>';
+            } else {
+                return null;
+            }
+           
+        }
+    } 
+  
+    
+    function renderRoutes($first_part){
+        $rutas = [
+            new Route('dashboard.php', 'Inicio', ['user', 'admin']),
+            new Route('lista.php', 'Peliculas', ['user', 'admin']),
+            new Route('agregar.php', 'Agregar', ['admin']),
+            new Route('logout.php', 'Logout', ['user', 'admin'])
+        ];
+        $i = 0;
+        while(sizeof($rutas)>$i) {
+            echo $rutas[$i]->getListElement($first_part);
+            $i += 1;
+        }
+    }
 ?>
 <nav class="navbar navbar-expand navbar-dark bg-dark">
     <img src="/catapeli/images/popcorn.png" width="30" height="30" >
     <a class="navbar-brand title" href="#">CataPeli</a>
     <ul class="navbar-nav">
-        <li class="nav-item <?php echo $first_part === 'dashboard.php' ? 'active' : ''; ?>">
-            <a class="nav-link" href="dashboard.php">Inicio</a>
-        </li>
-        <li class="nav-item <?php echo $first_part === 'lista.php' ? 'active' : ''; ?>">
-            <a class="nav-link" href="lista.php">Peliculas</a>
-        </li>
         <?php
-            if($_SESSION["role"] === 'admin') {
-                echo '<li class="nav-item">
-                <a class="nav-link" href="agregar.php">Agregar</a>
-            </li>';
-            }
+            renderRoutes($first_part);
         ?>
-        <li class="nav-item <?php echo $first_part === 'logout.php' ? 'active' : ''; ?>">
-            <a class="nav-link" href="logout.php">Logout</a>
-        </li>
     </ul>
 </nav>
 
