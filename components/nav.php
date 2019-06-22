d162ffead55bca4a0d3778c6b5168c40fd56ce92
lchapoy <luis_chapoy@hotmail.com>
2019-06-22 16:29:11 -0500

    agregar comentarios

--- components/nav.php
+++ components/nav.php
@@ -11,16 +11,15 @@
             $this->role = $role;
             $this->label = $label;
         }
-        //Aquí van los métodos
+        //Checa si la ruta actual esta activa
         public function rutaActiva($rutaActiva){
             return $this->ruta === $rutaActiva;
         }
+        //Checa si el usuario tiene permiso a la ruta
         public function permisoRole(){
             return in_array($_SESSION["role"] , $this->role);
         }
-        public function obtenerRuta(){
-            return $this->ruta;
-        }
+        //Obtiene HTML <li> para la ruta 
         public function getListElement($first_part) {
             if ($this->permisoRole()){
                 $active = $first_part === $this->ruta ? 'active' : '';
@@ -33,8 +32,9 @@
         }
     } 
   
-    
+    //Genera las rutas en la barra de navegacion dinamicamente.
     function renderRoutes($first_part){
+        //Crea los objectos de cada una de las rutas.
         $rutas = [
             new Route('dashboard.php', 'Inicio', ['user', 'admin']),
             new Route('lista.php', 'Peliculas', ['user', 'admin']),
@@ -42,7 +42,10 @@
             new Route('logout.php', 'Logout', ['user', 'admin'])
         ];
         $i = 0;
+        //Prueba usando while para iterar array
+        //Itera sobre las rutas creadas
         while(sizeof($rutas)>$i) {
+            //Imprime la ruta <li> obtenida de la funcion getListElement
             echo $rutas[$i]->getListElement($first_part);
             $i += 1;
         }
@@ -53,6 +56,7 @@
     <a class="navbar-brand title" href="#">CataPeli</a>
     <ul class="navbar-nav">
         <?php
+            //Imprime las rutas que el usuario tiene permiso.
             renderRoutes($first_part);
         ?>
     </ul>
